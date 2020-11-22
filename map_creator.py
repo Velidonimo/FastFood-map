a59e99783ceae3c794f0c838e008a8c54f99f760
Velidonimo <velidon@mail.ru>
2020-11-23 00:11:02 +0300

    _

--- map_creator.py
+++ map_creator.py
@@ -2,34 +2,58 @@ import pandas
 import folium
 
 
+def create_fg(df, name, color):
+    rest_names = df['name']
+    rest_cities = df["city"]
+    rest_lats = df['latitude']
+    rest_lons = df["longitude"]
+
+    html = """
+    <h4>{name}</h4>
+    <p>City: {city}.</p>
+    """
+
+    fg = folium.FeatureGroup(name)
+    for name, city, lat, lon in zip(rest_names, rest_cities, rest_lats, rest_lons):
+        iframe = folium.IFrame(html=html.format(name=name, city=city), width=170, height=100)
+        fg.add_child(folium.CircleMarker(location=[lat, lon], popup=folium.Popup(iframe),
+                                         color='grey',
+                                         fill_opacity=100,
+                                         fill_color=color))
+    return fg
+
+                                         # 'yellow' if name.lower() == "mcdonald's"
+                                         # else "red" if name.lower() in (
+                                         # "wendy's", "pizza hut", "burger king", "dairy queen")
+                                         # else "blue" if name.lower() == "domino's pizza"
+                                         # else "violet" if name.lower() == Taco Bell
+                                         # else "green" if name.lower() == "subway"
+                                         # else "grey"))
+
+
 df = pandas.read_csv('FastFoodRestaurants.csv')
-rest_names = df['name']
-rest_cities = df["city"]
-rest_lats = df['latitude']
-rest_lons = df["longitude"]
+feature_groups = []
 
-rest_map = folium.Map([30, -30], zoom_start=3)
-fg = folium.FeatureGroup('Restaurants')
+df_mc = df.loc[df["name"] == "McDonald's"]
+feature_groups.add(create_fg(df_mc, "McDonald's", 'yellow'))
 
-html = """
-<h4>{name}</h4>
-<p>City: {city}.</p>
-"""
+df_w = df.loc[df["name"] == "Wendy's"]
+feature_groups.add(create_fg(df_w, "Wendy's", "red"))
 
-for name, city, lat, lon in zip(rest_names, rest_cities, rest_lats, rest_lons):
-    iframe = folium.IFrame(html=html.format(name=name, city=city), width=170, height=100)
-    fg.add_child(folium.CircleMarker(location=[lat, lon], popup=folium.Popup(iframe),
-                                     color='grey',
-                                     fill_opacity=100,
-                                     fill_color='yellow' if name.lower() == "mcdonald's"
-                                            else "red" if name.lower() in ("wendy's", "pizza hut", "burger king", "dairy queen")
-                                            else "blue" if name.lower() == "domino's pizza"
-                                            else "violet" if name.lower() == "taco bell"
-                                            else "green" if name.lower() == "subway"
-                                            else "grey"))
+df_tb = df.loc[df["name"] == "Taco Bell"]
+feature_groups.add(create_fg(df_tb, "Taco Bell", "violet"))
+
+df_bk = df.loc[df["name"] == "Burger King"]
+feature_groups.add(create_fg(df_bk, "Burger King", "darkred"))
+
+chose = ("McDonald's", "Wendy's", "Taco Bell", "Burger King")
+df_others = df.loc[~df["name"].isin(chose)]
+feature_groups.add(create_fg(df_others, "Others", "grey"))
 
 
-rest_map.add_child(fg)
+for fg in feature_groups:
+    rest_map.add_child(fg)
+
 
 rest_map.save("rest_map.html")
 
