30d7a8e27ca7afa4d611309e6300a639c051c9bd
Velidonimo <velidon@mail.ru>
2020-11-23 01:10:25 +0300

    Sliced to layers

--- map_creator.py
+++ map_creator.py
@@ -3,6 +3,7 @@ import folium
 
 
 def create_fg(df, name, color):
+    """Create a featuregroup for restaurant"""
     rest_names = df['name']
     rest_cities = df["city"]
     rest_lats = df['latitude']
@@ -17,43 +18,37 @@ def create_fg(df, name, color):
     for name, city, lat, lon in zip(rest_names, rest_cities, rest_lats, rest_lons):
         iframe = folium.IFrame(html=html.format(name=name, city=city), width=170, height=100)
         fg.add_child(folium.CircleMarker(location=[lat, lon], popup=folium.Popup(iframe),
-                                         color='grey',
+                                         color=None,
                                          fill_opacity=100,
                                          fill_color=color))
     return fg
-
-                                         # 'yellow' if name.lower() == "mcdonald's"
-                                         # else "red" if name.lower() in (
-                                         # "wendy's", "pizza hut", "burger king", "dairy queen")
-                                         # else "blue" if name.lower() == "domino's pizza"
-                                         # else "violet" if name.lower() == Taco Bell
-                                         # else "green" if name.lower() == "subway"
-                                         # else "grey"))
 
 
 df = pandas.read_csv('FastFoodRestaurants.csv')
 feature_groups = []
 
-df_mc = df.loc[df["name"] == "McDonald's"]
-feature_groups.add(create_fg(df_mc, "McDonald's", 'yellow'))
+chosen = {"McDonald's": "yellow", "Wendy's": "red", "Taco Bell": "violet", "Burger King": "darkred"}
 
-df_w = df.loc[df["name"] == "Wendy's"]
-feature_groups.add(create_fg(df_w, "Wendy's", "red"))
 
-df_tb = df.loc[df["name"] == "Taco Bell"]
-feature_groups.add(create_fg(df_tb, "Taco Bell", "violet"))
+for name in chosen:
+    df_concrete = df.loc[df["name"] == name]
+    fg_concrete = create_fg(df_concrete, name, chosen[name])
+    # enable only a pair of restaurants
+    if name not in ("McDonald's", "Wendy's"): fg_concrete.show = False
+    feature_groups.append(fg_concrete)
 
-df_bk = df.loc[df["name"] == "Burger King"]
-feature_groups.add(create_fg(df_bk, "Burger King", "darkred"))
+df_others = df.loc[~df["name"].isin(chosen)]
+fg_others = create_fg(df_others, "Others", "grey")
+fg_others.show = False
+feature_groups.append(fg_others)
 
-chose = ("McDonald's", "Wendy's", "Taco Bell", "Burger King")
-df_others = df.loc[~df["name"].isin(chose)]
-feature_groups.add(create_fg(df_others, "Others", "grey"))
+rest_map = folium.Map([30, -30], zoom_start=3, tiles=None)
+folium.TileLayer(name="Restaurants").add_to(rest_map)
 
 
 for fg in feature_groups:
     rest_map.add_child(fg)
-
+rest_map.add_child(folium.LayerControl(collapsed=False))
 
 rest_map.save("rest_map.html")
 
